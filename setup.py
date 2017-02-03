e9df13c9c1d75bc738b2f12cf09a4e2f66a24c0f
Parham Pourdavood <ppourdavood@gmail.com>
2017-02-03 03:20:12 -0800

    Published on Pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='Autism_Treatment_Assistance',
+    packages=['Autism_Treatment_Assistance'],
+    version='0.2',
+    scripts=['Autism_Treatment_Assistance/ata'],
+    description="Employs data science and deep learning to assist therapists treat a common autism symptom that involves interpretation and correlation of facial and speech emotions",
+    long_description="Please visist Autism Treatment Assistance on Github: https://github.com/ParhamP/Autism_Treatment_Assistance",
+    author='Parham Pourdavood',
+    author_email='ppourdavood@gmail.com',
+    url='https://github.com/ParhamP/Autism_Treatment_Assistance',
+    download_url='https://github.com/ParhamP/Autism_Treatment_Assistance/tarball/0.1',
+    keywords=['data science, emotion recognition, psychology, autism, speech emotion recognition'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=['SimpleAudioIndexer', 'watson_developer_cloud',
+                      'SpeechRecognition', 'matplotlib', 'numpy'],
+    license="Apache-2.0"
+)
